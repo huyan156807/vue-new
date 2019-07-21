8428833de32d5f98368ec3feee75f41a8843e7f0
rose <1966025505@qq.com>
2019-07-21 17:04:27 +0800

    文件删除和修改初始化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: true,
+  devServer: {
+    // 实时保存、编译的配置段
+    open: true,
+    // 自动开启浏览器
+    host: '127.0.0.1',
+    // 服务主机名地址
+    port: 11106
+    // 服务运行端口
+  }
 }
